ed6f0f695c43c5f234f3c70c703d971f0596c137
Marouan el Marnissy <elmarnissymarouan@gmail.com>
2024-03-21 09:03:50 +0100

    Update index.php

--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -15,13 +15,30 @@
         <h1>Meldingen</h1>
         <a href="create.php">Nieuwe melding &gt;</a>
 
-        <?php if(isset($_GET['msg']))
-        {
+        <?php if(isset($_GET['msg'])){
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
-        <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
+        <div style="background: #ededed;">
+        <?php
+        require_once '../../../config/conn.php';
+        $query = "SELECT * FROM meldingen";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($meldingen as $melding) {
+            echo "<h1>" . $melding['attractie'] . "<h1>";
+
+        } 
+        ?>
+        
     </div>
+
+    <?php 
+    require_once 'conn.php';
+
+    ?>
 
 </body>
 
