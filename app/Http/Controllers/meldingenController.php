56ff0f2a0ec16a72f0f8280e6f6b7604c51f5f9d
Marouan el Marnissy <elmarnissymarouan@gmail.com>
2024-03-28 08:57:10 +0100

    iets
    dd

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -1,30 +1,38 @@
 <?php
 
-//Variabelen vullen
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
 $type = $_POST['type'];
+$overig = $_POST['overig'];
+
+if(empty($attractie)){$errors[]="Vul de attractie-naam in.";}
+
+if(!is_numeric($capaciteit)){$errors[]="Vul voor capaciteit een geldig getalin.";}
+
+if(isset($_POST['prioriteit'])) {
+    $prioriteit=1;
+}
+else{
+    $prioriteit=0;
+}
+
+if(isset($errors)){var_dump($errors);die();}
 
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
-
-//1. Verbinding
 require_once '../../../config/conn.php';
 
-//2. Query
-$query="INSERT INTO meldingen(attractie,type, melder, capaciteit, prioriteit)VALUES(:attractie,:type ,:melder, :capaciteit, :prioriteit)";
+$query="INSERT INTO meldingen(attractie,type, melder, capaciteit,prioriteit,overige_info)VALUES(:attractie,:type ,:melder,:capaciteit ,:prioriteit,:overig)";
 
-//3. Prepare
-$statement = $conn->prepare($query);
+$statement=$conn->prepare($query);
 
-//4. Execute
 $statement->execute([
-    ":attractie" => $attractie,
+    ":attractie"=>$attractie,
     ":type"=>$type,
-    ":melder" => $melder,
+    ":melder"=>$melder,
     ":capaciteit"=>$capaciteit,
     ":prioriteit"=>$prioriteit,
+    ":overig"=>$overig,
 ]);
 
 header(header: "Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -1,44 +1,66 @@
-<?php require_once __DIR__.'/../../../config/config.php'; ?>
+<?php require_once __DIR__ . '/../../../config/config.php'; ?>
 <!doctype html>
 <html lang="nl">
 
 <head>
     <title>StoringApp / Meldingen</title>
-    <?php require_once __DIR__.'/../components/head.php'; ?>
+    <?php require_once __DIR__ . '/../components/head.php'; ?>
 </head>
 
 <body>
 
-    <?php require_once __DIR__.'/../components/header.php'; ?>
+    <?php require_once __DIR__ . '/../components/header.php'; ?>
 
     <div class="container">
         <h1>Meldingen</h1>
         <a href="create.php">Nieuwe melding &gt;</a>
 
-        <?php if(isset($_GET['msg'])){
+        <?php if (isset($_GET['msg'])) {
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
         <div style="background: #ededed;">
-        <?php
-        require_once '../../../config/conn.php';
-        $query = "SELECT * FROM meldingen";
-        $statement = $conn->prepare($query);
-        $statement->execute();
-        $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($meldingen as $melding) {
-            echo "<h1>" . $melding['attractie'] . "<h1>";
+            <?php
+            require_once '../../../config/conn.php';
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+            ?>
 
-        } 
-        ?>
-        
+            <table>
+                <th>Attractie</th>
+                <th>Type</th>
+                <th>Melder</th>
+                <th>prioriteit</th>
+                <th>Overige Informatie</th>
+                <th>Aanpassen</th>
+                <?php foreach ($meldingen as $melding) : ?>
+                    <tr>
+                        <td><?php echo $melding['attractie']; ?></td>
+                        <td><?php echo $melding['type']; ?></td>
+                        <td><?php echo $melding['melder']; ?></td>
+                        <td><?php
+                        if($melding['prioriteit']==1){
+                            echo "Ja";
+                        }
+                        else{
+                            echo "Nee";
+                        }
+                         ?></td>
+                        <td><?php echo $melding['overige_info']; ?></td>
+                        <td><a href="edit.php?id=<?php echo $melding['id']; ?>">Aanpassen</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+
+
+
+        </div>
+
     </div>
-
-    <?php 
-    require_once 'conn.php';
-
-    ?>
 
 </body>
 
